94da45344844a2326ebb7dca1dc18f6d167f3104
lucaslng <saculgnuel1@gmail.com>
2024-11-11 09:48:26 -0500

    Add title

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Namer App',
+        title: 'Teachassist',
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: colorScheme,
