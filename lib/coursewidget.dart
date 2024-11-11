da5fe68ae2aedb5c34eeabf336082e4e728a52f7
lucaslng <saculgnuel1@gmail.com>
2024-11-10 23:21:43 -0500

    Show midterm and final marks

--- lib/coursewidget.dart
+++ lib/coursewidget.dart
@@ -12,62 +12,96 @@ class CourseWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
     return Card(
         elevation: 3,
         child: Padding(
           padding: const EdgeInsets.all(12.0),
           child: Builder(builder: (context) {
             if (course.code.contains("LUNCH")) {
-              return Column(children: [
+              return LunchWidget(course: course);
+            } else {
+              return CourseInfoWidget(course: course);
+            }
+          }),
+        ));
+  }
+}
+
+class CourseInfoWidget extends StatelessWidget {
+  const CourseInfoWidget({
+    super.key,
+    required this.course,
+  });
+
+  final Course course;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
                 Text(
-                  "LUNCH",
+                  "${course.code} - ${course.room}",
                   style: theme.textTheme.headlineSmall,
                 ),
+                if (course.name != "")
+                  Text(
+                    course.name,
+                    style: theme.textTheme.bodyLarge,
+                  ),
                 Text(
                   "Period ${course.period}",
                   style: theme.textTheme.bodyLarge,
                 ),
-              ]);
+                if (course.hasFinal || course.hasMidterm)
+                  Text(
+                    '${course.hasFinal ? "Final" : "Midterm"} Mark: ${course.finalOrMidtermMark}%',
+                    style: theme.textTheme.bodyLarge,
+                  )
+              ],
+            ),
+          ),
+          Builder(builder: (context) {
+            if (course.status() == "upcoming") {
+              return const SizedBox(width: 0);
+            } else if (!course.hasMark) {
+              return const SeeTeacherMessage();
+            } else if (course.isLevel()) {
+              return CourseLevelMessage(course: course);
             } else {
-              return Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "${course.code} - ${course.room}",
-                            style: theme.textTheme.headlineSmall,
-                          ),
-                          if (course.name != "")
-                            Text(
-                              course.name,
-                              style: theme.textTheme.bodyLarge,
-                            ),
-                          Text(
-                            "Period ${course.period}",
-                            style: theme.textTheme.bodyLarge,
-                          ),
-                        ],
-                      ),
-                    ),
-                    Builder(builder: (context) {
-                      if (course.status() == "upcoming") {
-                        return const SizedBox(width: 0);
-                      } else if (!course.hasMark) {
-                        return const SeeTeacherMessage();
-                      } else if (course.isLevel()) {
-                        return CourseLevelMessage(course: course);
-                      } else {
-                        return CourseMarkRadialGauge(course: course);
-                      }
-                    }),
-                  ]);
+              return CourseMarkRadialGauge(course: course);
             }
           }),
-        ));
+        ]);
+  }
+}
+
+class LunchWidget extends StatelessWidget {
+  const LunchWidget({
+    super.key,
+    required this.course,
+  });
+
+  final Course course;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Column(children: [
+      Text(
+        "LUNCH",
+        style: theme.textTheme.headlineSmall,
+      ),
+      Text(
+        "Period ${course.period}",
+        style: theme.textTheme.bodyLarge,
+      ),
+    ]);
   }
 }
 
